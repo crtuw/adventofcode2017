f0a8055e4043768f2d14882ed67eb7bd1920474f
cr <clemens@reichels.at>
2017-12-21 16:40:09 +0100

    day1 finished

--- day1.py
+++ day1.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# [int(1000*random.random())] for i in xrange(100)]
-myPuzzleInput = "1122"
+with open("day1.txt", "r") as myFile:
+    myPuzzleInput = myFile.readline()
+
 myList = [int(i) for i in myPuzzleInput]
 myList.append(myList[0])
 
 sum = 0
-for i in range(len(myList)-1):
-	if myList[i] == myList[i+1]:
-		sum = myList[i] + sum
+for i in range(len(myList) - 1):
+    if myList[i] == myList[i + 1]:
+        sum = myList[i] + sum
 print sum
-
