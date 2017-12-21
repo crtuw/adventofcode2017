9d1e541df36e6d7014dd4b2381d1ae6cb8e1b48e
cr <clemens@reichels.at>
2017-12-21 19:33:17 +0100

    appended day1task2

--- day1task2.py
+++ day1task2.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-with open("day1task1.txt", "r") as myFile:
+with open("day1task2.txt", "r") as myFile:
     myPuzzleInput = myFile.readline()
 
-myList = [int(i) for i in myPuzzleInput]
-myList.append(myList[0])
+myList = [int(i) for i in myPuzzleInput.strip()]
 
 sum = 0
 for i in range(len(myList) - 1):
-    if myList[i] == myList[i + 1]:
+    if myList[i] == myList[(i + len(myList) / 2) % len(myList)]:
         sum = myList[i] + sum
 print sum
