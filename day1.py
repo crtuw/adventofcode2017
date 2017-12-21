90f57fc5095c6854f795dc9671881365d7c38cee
cr <clemens@reichels.at>
2017-12-21 14:37:39 +0100

    day1 v0

---
+++ day1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+# [int(1000*random.random())] for i in xrange(100)]
+myPuzzleInput = "1122"
+myList = [int(i) for i in myPuzzleInput]
+myList.append(myList(0))
+
+sum = 0
+for i in range(len(myList)-1):
+	if myList[i] == myList[i+1]:
+		sum = myList[i] + sum
